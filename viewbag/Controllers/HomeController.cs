339280a9aacaf0351aac00df380376aa542f63c2
archies07 <138809776+archies07@users.noreply.github.com>
2023-08-01 12:34:54 +0530

    Add project files.

---
+++ viewbag/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using viewbag.Models;
+
+namespace viewbag.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from viewbag !!";
+            ViewBag.Currentdate = DateTime.Now.ToLongDateString();
+
+            string[] fruits = { "Apple", "banana", "Orange" };
+            ViewBag.FruitArray = fruits;
+
+            ViewBag.Sportslist = new List<string>()
+            {
+                "cricket",
+                "football",
+                "hockey",
+                "baseball"
+            };
+
+            Employee Archies= new Employee();
+            Archies.EmpId= 1;
+            Archies.EmpName = "Archies Patil";
+            Archies.EmpDesignation = "Software Developer";
+
+            ViewBag.Employee = Archies;
+
+            ViewBag.CommonMessage = "This message is accessible by both Viewbag and viewdata";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ viewbag/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace viewbag.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesignation { get; set; }
+    }
+}
